be6ba76fdc6f2da44146884965b2ef37253635a9
Snupai <yannluca@naeher.eu>
2023-06-22 13:55:40 +0200

    .

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'API_KEY', // Write here your API Key
+	weatherKey: '9a252e68581ed18e6c26ebe70e3125d3', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
